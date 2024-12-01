79e2e24e241bc979a2596539aad3b10de1c5cd5c
janhan42 <wkdgns720@naver.com>
2024-12-02 08:12:19 +0900

    m1이라 그런가?

---
+++ srcs/requirements/mariadb/mariadb-init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+chown -R mysql:mysql /var/lib/mysql
+
+service mariadb start
+
+while ! mysqladmin ping -h localhost --silent; do
+	sleep 1
+done
+
+echo "$DB_NAME $DB_USER, $DB_PASSWORD, $DB_HOST"
+echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > init.sql
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' ;" >> init.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
+
+mysql < init.sql
+
+service mariadb stop
+while mysqladmin ping -h localhost --silent; do
+	sleep 1
+done
+
+mariadb
